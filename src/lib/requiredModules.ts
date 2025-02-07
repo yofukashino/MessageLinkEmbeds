b72313510d130aff4d1244dc4c3bdd4f3e843e48
yofukashino <alokender018@gmail.com>
2025-02-07 06:37:14 +0530

    fix: module

--- src/lib/requiredModules.ts
+++ src/lib/requiredModules.ts
@@ -1,31 +1,25 @@
 import { webpack } from "replugged";
+import Utils from "./utils";
 import Types from "../types";
 
 export const Modules: Types.Modules = {};
 
 Modules.loadModules = async (): Promise<void> => {
-  Modules.DiscordConstantsModule = await webpack
-    .waitForModule<Types.GenericModule>(webpack.filters.bySource(".MFA_WARNING="), {
+  Modules.DiscordConstants = await webpack
+    .waitForModule<Types.DefaultTypes.RawModule>(webpack.filters.bySource(".HYPESQUAD=4"), {
       timeout: 10000,
+      raw: true,
     })
+    .then((v) =>
+      Utils.unmangleExports<Types.DiscordConstants>(v, {
+        Permissions: ["ADMINISTRATOR", "MANAGE_GUILD"],
+        ChannelTypes: ["GUILD_TEXT", "GUILD_VOICE"],
+        Endpoints: ["AUTH_SESSIONS", "AUTH_SESSIONS_LOGOUT"],
+      }),
+    )
     .catch(() => {
       throw new Error("Failed To Find DiscordConstants Module");
     });
-
-  Modules.DiscordConstants = {
-    Permissions: webpack.getExportsForProps(Modules.DiscordConstantsModule, [
-      "ADMINISTRATOR",
-      "MANAGE_GUILD",
-    ]),
-    ChannelTypes: webpack.getExportsForProps(Modules.DiscordConstantsModule, [
-      "GUILD_TEXT",
-      "GUILD_VOICE",
-    ]),
-    Endpoints: webpack.getExportsForProps(Modules.DiscordConstantsModule, [
-      "AUTH_SESSIONS",
-      "AUTH_SESSIONS_LOGOUT",
-    ]),
-  };
 
   Modules.MessageAccessories ??= await webpack
     .waitForModule<Types.MessageAccessories>(webpack.filters.bySource("this.renderEmbeds"), {
@@ -66,24 +60,43 @@ Modules.loadModules = async (): Promise<void> => {
       throw new Error("Failed To Find ChannelMessage Module");
     });
 
-  Modules.IconUtils ??= await webpack.waitForProps<Types.IconUtils>("getUserAvatarURL");
+  Modules.IconUtils ??= await webpack
+    .waitForProps<Types.IconUtils>(["getUserAvatarURL"], {
+      timeout: 1000,
+    })
+    .catch(() => {
+      throw new Error("Failed To Find IconUtils Module");
+    });
 
   Modules.MessageDisplayCompact ??= await webpack
-    .waitForModule<Types.ChatSettingUtils>(webpack.filters.bySource("),useSetting:()=>"))
+    .waitForModule<Types.ChatSettingUtils>(webpack.filters.bySource("),useSetting:"), {
+      timeout: 1000,
+    })
     .then((mod) =>
       webpack.getFunctionBySource<(...args: unknown[]) => Types.ChatSettingUtils>(
         mod,
-        "),useSetting:()=>",
+        "),useSetting:",
       )("textAndImages", "messageDisplayCompact", (res) => res?.value),
-    );
+    )
+    .catch(() => {
+      throw new Error("Failed To Find MessageDisplayCompact Module");
+    });
 
   Modules.MessageClasses = {
-    ...(await webpack.waitForProps<Types.SearchMessageClasses>("message", "searchResult")),
-    ...(await webpack.waitForProps<Types.EmbedClasses>(
-      "embedAuthorIcon",
-      "embedAuthor",
-      "embedAuthor",
-    )),
+    ...(await webpack
+      .waitForProps<Types.SearchMessageClasses>(["message", "searchResult"], {
+        timeout: 1000,
+      })
+      .catch(() => {
+        throw new Error("Failed To Find SearchMessageClasses Module");
+      })),
+    ...(await webpack
+      .waitForProps<Types.EmbedClasses>(["embedAuthorIcon", "embedAuthor", "embedAuthor"], {
+        timeout: 1000,
+      })
+      .catch(() => {
+        throw new Error("Failed To Find EmbedClasses Module");
+      })),
   };
 
   Modules.MessageCacheActions ??= await webpack
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { util } from "replugged";
+import { util, webpack } from "replugged";
 import {
   api as APIRequestUtils,
   fluxDispatcher as FluxDispatcher,
@@ -158,6 +158,59 @@ export const fetchMessage = async ({ channelId, messageId }): Promise<Types.Mess
   return message;
 };
 
+export const isObject = (testMaterial: unknown): boolean =>
+  typeof testMaterial === "object" && !Array.isArray(testMaterial) && testMaterial != null;
+
+export const unmangleExports = <T>(
+  moduleFilter: Types.DefaultTypes.Filter | Types.DefaultTypes.RawModule,
+  map: Record<string, string | string[] | RegExp | Types.DefaultTypes.AnyFunction>,
+): T => {
+  const getExportKeyFinder = (
+    mapValue: string | string[] | RegExp | Types.DefaultTypes.AnyFunction,
+  ): Types.DefaultTypes.AnyFunction => {
+    if (typeof mapValue === "function") {
+      return (mod: Types.DefaultTypes.RawModule["exports"]) => {
+        return mapValue(mod);
+      };
+    }
+
+    if (Array.isArray(mapValue)) {
+      return (mod: Types.DefaultTypes.RawModule["exports"]) => {
+        if (!isObject(mod)) return "";
+        for (const [k, exported] of Object.entries(mod)) {
+          if (mapValue.every((p) => Object.hasOwnProperty.call(exported, p))) return k;
+        }
+      };
+    }
+
+    return (mod: Types.DefaultTypes.RawModule["exports"]) =>
+      webpack.getFunctionKeyBySource(mod, mapValue as string);
+  };
+
+  const mod: Types.DefaultTypes.RawModule =
+    typeof moduleFilter === "function"
+      ? webpack.getModule(moduleFilter, { raw: true })
+      : moduleFilter;
+
+  if (!mod) return {} as T;
+
+  const unmangled = {} as T;
+
+  for (const key in map) {
+    const findKey = getExportKeyFinder(map[key]);
+    const valueKey = findKey(mod.exports) as string;
+    Object.defineProperty(unmangled, key, {
+      get: () => mod.exports[valueKey],
+      set: (v) => {
+        mod.exports[valueKey] = v;
+      },
+    });
+  }
+
+  // Return the unmangled object
+  return unmangled;
+};
+
 export default {
   ...util,
   LimitedMap,
@@ -169,4 +222,6 @@ export default {
   formatEmptyContent,
   resizeToFit,
   fetchMessage,
+  isObject,
+  unmangleExports,
 };
